0c532c660d3a4a98087f5a3f3cdcf0e348f30873
Martin Heidegger <martin.heidegger@gmail.com>
2020-12-21 18:17:00 +0900

    feat: exporting msgpack options for easier use

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,21 @@
 import { NamedCodec } from 'codecs'
-import { encode, decode } from '@msgpack/msgpack'
+import * as msg from '@msgpack/msgpack'
 
-type input = Parameters<typeof encode>
-type output = ReturnType<typeof decode>
+type input = Parameters<typeof msg.encode>
+type output = ReturnType<typeof msg.decode>
 
 /* eslint-disable @typescript-eslint/no-redeclare */
 declare namespace msgpack {
   interface MsgPackCodec extends NamedCodec<'msgpack', input[0], output> {
     encode: (input: input[0], options?: input[1]) => Buffer
-    decode: (input: Buffer, options?: Parameters<typeof decode>[1]) => output
+    decode: (input: Buffer, options?: Parameters<typeof msg.decode>[1]) => output
   }
+  type Decoder <ContextType> = msg.Decoder<ContextType>
+  type DecodeOptions <ContextType = undefined> = msg.DecodeOptions<ContextType>
+  type Encoder <ContextType> = msg.Encoder<ContextType>
+  type EncodeOptions <ContextType = undefined> = msg.EncodeOptions<ContextType>
+  type ExtData = msg.ExtData
+  type ExtensionCodec <ContextType> = msg.ExtensionCodec<ContextType>
 }
 
 declare const msgpack: msgpack.MsgPackCodec
