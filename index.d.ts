7b9a383efa4e463f657944fe4624f9228e721fc0
Martin Heidegger <martin.heidegger@gmail.com>
2020-12-21 18:27:59 +0900

    fix: allowing extended MsgPackCodec typescript

--- index.d.ts
+++ index.d.ts
@@ -6,7 +6,7 @@ type output = ReturnType<typeof msg.decode>
 
 /* eslint-disable @typescript-eslint/no-redeclare */
 declare namespace msgpack {
-  interface MsgPackCodec extends NamedCodec<'msgpack', input[0], output> {
+  interface MsgPackCodec <Name extends string = 'msgpack'> extends NamedCodec<Name, input[0], output> {
     encode: (input: input[0], options?: input[1]) => Buffer
     decode: (input: Buffer, options?: Parameters<typeof msg.decode>[1]) => output
   }
